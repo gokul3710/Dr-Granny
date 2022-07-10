d35001308f1f32958c7329be7ecd965ed46a41b0
gokul3710 <107317309+gokul3710@users.noreply.github.com>
2022-07-10 19:35:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("sworker.js").then(registration=>{
+    navigator.serviceWorker.register("./sworker.js").then(registration=>{
         console.log("Service worker registered");
         console.log(registration);
     }).catch(error=>{
